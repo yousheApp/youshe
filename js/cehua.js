eec04b19043965d3cbdb520c66db4aa0530493c0
[ly453890128] <43500876@qq.com>
2017-03-04 11:48:36 +0800

    ly

---
+++ js/cehua.js
@@ -0,0 +1,17 @@
+
+
+//侧边栏滑动出现效果
+//
+var myself = document.querySelector(".self");
+var mask = document.querySelector(".mask");
+var side = document.querySelector(".left-box");
+
+myself.addEventListener("touchstart",function(){
+	side.style.left=0;
+	mask.style.display="block";
+})
+
+mask.addEventListener("touchstart",function(){
+	side.style.left="-6.3rem";
+	mask.style.display="none";
+})
